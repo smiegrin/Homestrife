31158e890348f8d80b3fb9d7a5f8fa11ca13c0db
Smie Grin <smie.grin@gmail.com>
2016-05-16 17:14:30 +0200

    Implements abstract Fighter base class.

---
+++ Fighter.cpp
@@ -0,0 +1,50 @@
+#include "Fighter.h"
+
+int Fighter::getXPos() { return x; }
+
+int Fighter::getYPos() { return y; }
+
+int Fighter::getWidth() { return width; }
+
+int Fighter::getHeight() { return height; }
+
+int Fighter::getXVel() { return xVel; }
+
+int Fighter::getYVel() { return yVel; }
+
+int Fighter::getDirection() { return direction; }
+
+int Fighter::getAnim() { return anim; }
+
+int Fighter::getHealthNum() { return health; }
+
+int Fighter::getMaxHealth() { return maxHealth; }
+
+int Fighter::getHealthPercent() { return 100*health/maxHealth; }
+
+int Fighter::getDefense() { return defense; }
+
+int Fighter::getPower() { return power; }
+
+int Fighter::getAttackSpeed() { return attackSpeed; }
+
+int Fighter::getMoveSpeed() { return moveSpeed; }
+
+bool Fighter::hitAt(int hitX, int hitY, int hitPower = 0) {
+    if (hitX >= x && hitX <= x + width && hitY >= y && hitY <= y + width) {
+        health -= hitPower - defense;
+        if (health < 0) health = 0;
+        return true;
+    }
+    return false;
+}
+
+void Fighter::setPosition(int xP, int yP) {
+    x = xP;
+    y = yP;
+}
+
+void Fighter::setVelocity(int xV, int yV) {
+    xVel = xV;
+    yVel = yV;
+}
---
+++ Fighter.h
@@ -0,0 +1,52 @@
+#ifndef FIGHTER_H
+#define FIGHTER_H
+
+#include "GameObject.h"
+
+class Fighter : public GameObject {
+public: enum States {READY, READY_AIR, HIGH, HIGH_AIR, LOW, LOW_AIR, DOWN, DOWN_AIR, KO, KO_AIR};
+        enum Input {JUMP, GO_LEFT, GO_RIGHT, STOP_LEFT, STOP_RIGHT, ATTACK_LOW, ATTACK_HIGH};
+private:
+    int x;
+    int y;
+    int width;
+    int height;
+    int xVel;
+    int yVel;
+    int direction;
+    int anim;
+    int health;
+    int maxHealth;
+    int defense;
+    int power;
+    int attackSpeed;
+    int attackCooldown;
+    int moveSpeed;
+public:
+    int getXPos();
+    int getYPos();
+    int getWidth();
+    int getHeight();
+    int getXVel();
+    int getYVel();
+    int getDirection();
+    int getAnim();
+    int getHealthNum();
+    int getMaxHealth();
+    int getHealthPercent();
+    int getDefense();
+    int getPower();
+    int getAttackSpeed();
+    int getMoveSpeed();
+    bool isAlive();
+
+    bool hitAt(int,int,int);
+
+    virtual int logic() = 0;
+    virtual void input(Input) = 0;
+    void setPosition(int,int);
+    void setVelocity(int,int);
+
+};
+
+#endif // FIGHTER_H
