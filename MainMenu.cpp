e974fb430d87c76b3feb720c077c4d1b97fabaae
Smie Grin <smie.grin@gmail.com>
2016-05-20 11:46:37 +0200

    Adds simple animation to title on Main Menu.

--- MainMenu.cpp
+++ MainMenu.cpp
@@ -2,6 +2,7 @@
 #include "Button.h"
 #include "ResourceManager.h"
 #include "CharacterScreen.h"
+#include <math.h>
 
 MainMenu::MainMenu() {
     playButton = Button(325,400,150,50,sf::Color(59,200,73,255),"PLAY");
@@ -12,11 +13,20 @@ MainMenu::MainMenu() {
 }
 
 int MainMenu::open(sf::RenderWindow* window) {
-    ResourceManager::SongTemp.play();
+    //ResourceManager::SongTemp.play();
+    int anim = 0;
     while (window->isOpen()) {
         sf::Event event;
         while(window->pollEvent(event)) {
             if(event.type == sf::Event::Closed) window->close();
+            if(event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Escape) window->close();
+                if (event.key.code == sf::Keyboard::Return) {
+                    ResourceManager::SongTemp.stop();
+                    CharacterScreen().open(window); //play the game.
+                    ResourceManager::SongTemp.play();
+                }
+            }
             if(event.type == sf::Event::MouseButtonPressed) {
                 if(playButton.pointOnBox(event.mouseButton.x, event.mouseButton.y)) {
                     ResourceManager::SongTemp.stop();
@@ -25,6 +35,8 @@ int MainMenu::open(sf::RenderWindow* window) {
                 }
             }
         }
+
+        if(anim <= 120) title.setPosition(5,pow(anim - 120,3)/-1728), anim++;
 
         window->clear(sf::Color(0,186,255,255));
         playButton.drawSelf(window);
